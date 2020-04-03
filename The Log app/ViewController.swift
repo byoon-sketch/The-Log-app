861a9b83bb056e80aa44badc1dc62461146c7582
Benjamin Yoon <byoon@trinitypawling.org>
2020-04-02 21:00:18 -0400

    Initial Commit

---
+++ The Log app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Log app
+//
+//  Created by Ben Yoon on 4/2/20.
+//  Copyright © 2020 Benjamin Yoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
